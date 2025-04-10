b7760d6b5d6c842904245144273eff9484e54074
maxkidflash <jhaynes4@my.waketech.edu>
2025-04-10 17:56:10 -0400

    gradebook.js
    updated code

--- gradebook.js
+++ gradebook.js
@@ -1,14 +1,14 @@
 //.TODO: Fetch.data.from the .PostgreSQL.database.(to.be.implemented later)
 function fetchGradeData() {
-//.This.function.will.query the PostgreSQL .database and return grade data
-console.log("Fetching grade data ... ");
+     //.This.function.will.query the PostgreSQL .database and return grade data
+     console.log("Fetching grade data ... ");
 
 }
 
 //.TODO: Populate the table with grade data
 function populateGradebook(data) {
-//.This .function will take the fetched grade data and populate the table
-console.log("Populating gradebook with data:", data);
+    //.This .function will take the fetched grade data and populate the table
+    console.log("Populating gradebook with data:", data);
 
 }
 
